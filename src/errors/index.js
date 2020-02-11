7198b7cfa262fb12d8ea7a10012d86b09f4bd1bd
nardhar <nardhar.mure@gmail.com>
2020-02-11 10:10:28 -0400

    exporting AuthorizationError and ForbiddenError

--- src/errors/index.js
+++ src/errors/index.js
@@ -2,6 +2,8 @@ const FieldError = require('./field-error');
 const ApiError = require('./api-error');
 const NotFoundError = require('./not-found-error');
 const ValidationError = require('./validation-error');
+const ForbiddenError = require('./forbidden-error');
+const AuthorizationError = require('./authorization-error');
 const handler = require('./handler');
 
 module.exports = {
@@ -9,5 +11,7 @@ module.exports = {
   ApiError,
   NotFoundError,
   ValidationError,
+  ForbiddenError,
+  AuthorizationError,
   handler,
 };

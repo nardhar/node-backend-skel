05266e35a088348054accf2daeceb277afda2856
nardhar <nardhar.mure@gmail.com>
2020-02-11 09:34:15 -0400

    removing sequelize requirement from ValidationError constructor

--- src/errors/validation-error.js
+++ src/errors/validation-error.js
@@ -1,4 +1,3 @@
-const { ValidationError: SequelizeValidationError } = require('sequelize');
 const ApiError = require('./api-error');
 const FieldError = require('./field-error');
 
@@ -12,18 +11,6 @@ class ValidationError extends ApiError {
         return error instanceof FieldError
           ? error
           : new FieldError(error.field || null, error.code || '', error.args || []);
-      });
-    } else if (errors instanceof SequelizeValidationError) {
-      // errors.errors is an Array of sequelize.ValidationErrorItem
-      this.data = (errors.errors || []).map((err) => {
-        return new FieldError(
-          // TODO: check if path is the actual field
-          err.path,
-          // TODO: check if validatorKey is a i18n code for building the fieldError
-          err.validatorKey,
-          // TODO: check for all posible values of err.validatorArgs
-          (err.value ? [err.value] : []).concat(err.validatorArgs),
-        );
       });
     } else {
       this.data = [];
